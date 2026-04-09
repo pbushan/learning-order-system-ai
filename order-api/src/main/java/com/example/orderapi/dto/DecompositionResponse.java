9cb4a12ca236b2caa5b2c7267ba0bff0c9adedb5
Phani Bushan <pbushan@yahoo.com>
2026-04-08 21:44:18 -0400

    Use primitive boolean for decompositionComplete

--- order-api/src/main/java/com/example/orderapi/dto/DecompositionResponse.java
+++ order-api/src/main/java/com/example/orderapi/dto/DecompositionResponse.java
@@ -13,8 +13,7 @@ public class DecompositionResponse {
     private String requestId;
 
     @JsonProperty("decompositionComplete")
-    @NotNull(message = "decompositionComplete is required")
-    private Boolean decompositionComplete;
+    private boolean decompositionComplete;
 
     @JsonProperty("stories")
     @NotNull(message = "stories is required")
@@ -29,11 +28,11 @@ public class DecompositionResponse {
         this.requestId = requestId;
     }
 
-    public Boolean getDecompositionComplete() {
+    public boolean getDecompositionComplete() {
         return decompositionComplete;
     }
 
-    public void setDecompositionComplete(Boolean decompositionComplete) {
+    public void setDecompositionComplete(boolean decompositionComplete) {
         this.decompositionComplete = decompositionComplete;
     }
 
